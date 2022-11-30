adb867dd6413d77e7e065951bec504a1cdb1ca21
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-01 01:17:02 +0900

    feat(TransactionConfig): 트랜잭션 관리 설정

---
+++ module-member/src/main/java/com/gaethering/modulemember/config/TransactionConfig.java
@@ -0,0 +1,8 @@
+package com.gaethering.modulemember.config;
+
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+public class TransactionConfig {
+
+}
