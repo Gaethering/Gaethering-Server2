026d74a254e22bfbbd8d554d25d2eb38af821294
naekang <rlawlsgh6306@gmail.com>
2022-11-30 21:39:44 +0900

    feat: 인증 코드 전송 API controller 작성

---
+++ module-member/src/main/java/com/gaethering/modulemember/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.gaethering.modulemember.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/email-auth")
+    public ResponseEntity<Void> sendEmailAuthCode(@RequestBody EmailAuthRequest emailAuthRequest) {
+        memberService.sendEmailAuthCode(emailAuthRequest.getEmail());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
