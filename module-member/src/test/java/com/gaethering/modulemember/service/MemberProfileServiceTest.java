ffe77d4dd657b75dc9cbd371f35d7a0fe5f30184
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-01 01:14:57 +0900

    test(MemberProfileServiceTest): 회원(본인) 프로필 조회 테스트 코드 작성

---
+++ module-member/src/test/java/com/gaethering/modulemember/service/MemberProfileServiceTest.java
@@ -0,0 +1,97 @@
+package com.gaethering.modulemember.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+import com.gaethering.moduledomain.domain.member.Member;
+import com.gaethering.moduledomain.domain.member.MemberProfile;
+import com.gaethering.moduledomain.domain.member.Pet;
+import com.gaethering.moduledomain.domain.type.Gender;
+import com.gaethering.moduledomain.repository.member.MemberRepository;
+import com.gaethering.modulemember.dto.OwnProfileResponse;
+import com.gaethering.modulemember.exception.errorcode.MemberErrorCode;
+import com.gaethering.modulemember.exception.member.MemberNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MemberProfileServiceTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+    @InjectMocks
+    private MemberProfileServiceImpl memberProfileService;
+
+    private List<Pet> pets;
+    private MemberProfile memberProfile;
+    private Member member;
+
+    @BeforeEach
+    public void setUP() {
+        Pet pet1 = Pet.builder()
+            .id(1L)
+            .name("pet1")
+            .isRepresentative(true)
+            .build();
+        Pet pet2 = Pet.builder()
+            .id(2L)
+            .name("pet2")
+            .isRepresentative(false)
+            .build();
+        pets = List.of(pet1, pet2);
+        memberProfile = MemberProfile.builder()
+            .id(1L)
+            .gender(Gender.MALE)
+            .phoneNumber("010-0000-0000")
+            .mannerDegree(36.5f)
+            .build();
+        member = Member.builder()
+            .id(1L)
+            .email("member1@test.com")
+            .memberProfile(memberProfile)
+            .pets(pets)
+            .build();
+    }
+
+    @Test
+    public void getOwnProfileFailure() {
+        //given
+        given(memberRepository.findByEmail(anyString()))
+            .willReturn(Optional.empty());
+
+        //when
+        //then
+        MemberNotFoundException exception = assertThrows(
+            MemberNotFoundException.class, () -> memberProfileService.getOwnProfile(anyString()));
+        assertThat(exception.getErrorCode()).isEqualTo(MemberErrorCode.MEMBER_NOT_FOUND);
+    }
+
+    @Test
+    public void getOwnProfileSuccess() {
+        //given
+        given(memberRepository.findByEmail(anyString()))
+            .willReturn(Optional.of(member));
+
+        //when
+        OwnProfileResponse profile = memberProfileService.getOwnProfile(anyString());
+
+        //then
+        assertThat(profile.getEmail()).isEqualTo(member.getEmail());
+        assertThat(profile.getNickname()).isEqualTo(member.getNickname());
+        assertThat(profile.getPhoneNumber()).isEqualTo(member.getMemberProfile().getPhoneNumber());
+        assertThat(profile.getGender()).isEqualTo(member.getMemberProfile().getGender());
+        assertThat(profile.getMannerDegree()).isEqualTo(
+            member.getMemberProfile().getMannerDegree());
+        assertThat(profile.getPetCount()).isEqualTo(member.getPets().size());
+        assertThat(profile.getPets().get(0).getId()).isEqualTo(member.getPets().get(0).getId());
+        assertThat(profile.getPets().get(1).getId()).isEqualTo(member.getPets().get(1).getId());
+    }
+}
