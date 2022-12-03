53b3f8e930fcf62c95f19a78942be01dbf172933
EunjunOh123 <grace99985@gmail.com>
2022-12-03 20:47:11 +0900

    feat: 좋아요 엔티티 생성

---
+++ module-domain/src/main/java/com/gaethering/moduledomain/domain/board/Heart.java
@@ -0,0 +1,31 @@
+package com.gaethering.moduledomain.domain.board;
+
+import com.gaethering.moduledomain.domain.member.Member;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Heart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "heart_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", updatable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", updatable = false)
+    private Post post;
+
+    public void setPost (Post post) {
+        this.post = post;
+    }
+}
