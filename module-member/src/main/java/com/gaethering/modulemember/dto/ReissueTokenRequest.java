3a18befa027c77a2c394a80f8ec981321b756ac7
EunjunOh123 <grace99985@gmail.com>
2022-12-05 00:10:18 +0900

    feat: access token 재발급 요청 및 응답 dto 생성

---
+++ module-member/src/main/java/com/gaethering/modulemember/dto/ReissueTokenRequest.java
@@ -0,0 +1,17 @@
+package com.gaethering.modulemember.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ReissueTokenRequest {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+}
---
+++ module-member/src/main/java/com/gaethering/modulemember/dto/ReissueTokenResponse.java
@@ -0,0 +1,12 @@
+package com.gaethering.modulemember.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReissueTokenResponse {
+
+    private String accessToken;
+
+}
