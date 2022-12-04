5fd0fc0eb34109c7a8c1a321db33ba3f1598bc1c
bokyung <qhrud7915@naver.com>
2022-12-04 20:25:08 +0900

    refactor: 펫 프로필 이미지 수정 기능 구현
    Pet Entity 수정
    Pet Controller, Service 생성

---
+++ module-member/src/main/java/com/gaethering/modulemember/service/pet/PetServiceImpl.java
@@ -0,0 +1,39 @@
+package com.gaethering.modulemember.service.pet;
+
+import com.gaethering.moduledomain.domain.member.Pet;
+import com.gaethering.moduledomain.repository.pet.PetRepository;
+import com.gaethering.modulemember.exception.pet.PetNotFoundException;
+import com.gaethering.modulemember.service.ImageUploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PetServiceImpl implements PetService {
+
+    private final ImageUploadService imageUploadService;
+    private final PetRepository petRepository;
+
+    @Value("${default.image-url}")
+    private String defaultImageUrl;
+
+    @Override
+    public String updatePetImage(Long id, MultipartFile multipartFile) {
+        Pet pet = petRepository.findById(id)
+                .orElseThrow(PetNotFoundException::new);
+
+        if (!defaultImageUrl.equals(pet.getImageUrl())) {
+            imageUploadService.removePetImage(pet.getImageUrl());
+        }
+
+        String newImageUrl = imageUploadService.uploadPetImage(multipartFile);
+        pet.updateImage(newImageUrl);
+
+        return newImageUrl;
+    }
+
+}
